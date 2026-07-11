c798628212fcc8ab211eab5315c6cb154f752ac7
nutrijesicaaruge-byte <nutrijesicaaruge@gmail.com>
2026-07-11 19:27:26 -0300

    Delete java prueba/script1.js

--- java prueba/script1.js
+++
@@ -1,9 +0,0 @@
-//calculo del precio de una cartera
-
-let precioCosto= 235;
-let ganancia= precioCosto * 0.25;
-let impuesto= (precioCosto + ganancia) * 0.12;
-let descuento= (precioCosto + ganancia + impuesto) * 0.10;
-let precioFinal= precioCosto + ganancia + impuesto - descuento;
-
-console.log (precioFinal);
